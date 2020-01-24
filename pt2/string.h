abd07e278a08912085a7e9bb06a692420597fdbb
Andy <wrestlngfanatic@gmail.com>
2020-01-24 18:46:14 -0500

    last edits

--- pt2/string.h
+++ pt2/string.h
@@ -1,85 +1,44 @@
 #pragma once
 #include "object.h"
-#include <iostream>
 
 class String : public Object
 {
 public:
-	char *val_;
-	size_t size_;
+	char *val_;				 // the array of chars to be represented as a string
+	long unsigned int size_; // the size of the String
 
+	// Default constructor
 	String()
 	{
-		size_ = 0;
-		val_ = new char[size_];
 	}
 
+	// Constructor given an array of chars to initialize a String with
 	String(const char *hold)
 	{
-		size_ = sizeof(hold);
-		val_ = new char[size_];
-
-		for (size_t i = 0; i < size_; i++)
-		{
-			val_[i] = hold[i];
-		}
 	}
 
+	// Deconstructor
 	~String()
 	{
-		delete[] val_;
 	}
 
-	size_t hash_me()
+	// Generates a hash value for this Object
+	long unsigned int hash_me()
 	{
-		size_t res = 0;
-
-		for (size_t i = 0; i < size_; i++)
-		{
-			res = res + val_[i];
-		}
-
-		return res;
 	}
 
+	// Checks for equality between this Queue and the Object passed in
 	bool equals(Object *o)
 	{
-		if (o == nullptr)
-			return false;
-		String *s = dynamic_cast<String *>(o);
-		if (s == nullptr)
-			return false;
-
-		return strcmp(s->val_) == 0;
 	}
 
+	// Concatenates two Strings together
 	String *concat(String *end)
 	{
-		size_t newSize = this->size_ + end->size_;
-		char *newVal = new char[newSize];
-
-		for (size_t i = 0; i < this->size_; i++)
-		{
-			newVal[i] = this->val_[i];
-		}
-
-		for (size_t j = this->size_; j < newSize; j++)
-		{
-			newVal[j] = end->val_[j - this->size_];
-		}
-
-		String *result = new String(newVal);
-		return result;
 	}
 
+	// Compares the values of two Strings alphanumerically
 	int strcmp(char *val)
 	{
-		for (size_t i = 0; i < size_; i++)
-		{
-			if (val[i] != this->val_[i])
-			{
-				return this->val_[i] - val[i];
-			}
-		}
 	}
 };
